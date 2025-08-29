f1837d8294ec12f413d8a9680cc210936f4b00ad
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-08-29 12:32:11 +0800

    Added JS for getting DOM elements and changing their text content

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,17 @@ const weatherAPI =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 const key = "43GQQ65K28BCAUG6QME297TQD";
 
+const locationText = document.querySelector("#location > h2");
+const dateText = document.querySelector("#date");
+const timeText = document.querySelector("#time");
+const tempRealText = document.querySelector("#temp-real");
+const tempUnitText = document.querySelector("#temp-unit");
+const conditionText = document.querySelector("#condition");
+const feelText = document.querySelector("#feel");
+
+const nextDayDisplays = document.querySelectorAll(".next-day");
+const nextHourDisplays = document.querySelectorAll(".next-hour");
+
 async function fetchWeatherData(city) {
   const response = await fetch(
     `${weatherAPI}${city}?unitGroup=us&key=${key}&contentType=json`,
@@ -12,7 +23,32 @@ async function fetchWeatherData(city) {
       headers: {},
     },
   );
-  const data = await response.json();
+
+  return await response.json();
 }
 
-// fetchWeatherData("Tokyo");
+async function displayWeatherData(data) {
+  locationText.textContent = "Tokyo";
+  dateText.textContent = "Wed, June 18";
+  timeText.textContent = "12:01";
+  tempRealText.textContent = "75°";
+  tempUnitText.textContent = "F";
+  conditionText.textContent = "Sunny";
+  feelText.textContent = "Feels like 77°";
+
+  nextDayDisplays.forEach((display) => {
+    display.querySelector(".next-dayname").textContent = "Wed";
+    display.querySelector(".next-date").textContent = "June 19";
+    // display.querySelector(".next-icon");
+    display.querySelector(".next-temp").textContent = "72°";
+  });
+
+  nextHourDisplays.forEach((display) => {
+    display.querySelector(".next-hourname").textContent = "13:01";
+    // display.querySelector(".next-icon");
+    display.querySelector(".next-temp").textContent = "77°";
+  });
+}
+
+document.onload = displayWeatherData("Jay");
+// console.log(await fetchWeatherData("Tokyo"));
